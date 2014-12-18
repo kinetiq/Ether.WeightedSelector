2e7f706f124fc0365ad57c47a29c6ae02fb7ba76
Brian <brianm@getkinetiq.com>
2014-12-18 08:28:13 -0500

    Removed using statements referencing async, to try and improve support for monodevelop.

--- Ether.WeightedSelector.Tests/MultiSelect_NoDuplicates_Tests.cs
+++ Ether.WeightedSelector.Tests/MultiSelect_NoDuplicates_Tests.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Ether.WeightedSelector.Tests.Helpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
--- Ether.WeightedSelector/Algorithm/BinarySearchOptimizer.cs
+++ Ether.WeightedSelector/Algorithm/BinarySearchOptimizer.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Ether.WeightedSelector.Algorithm
 {
